df475e45ed7cfb090277f294fa7d8a93f0fdf5a9
Ueda-Masashi <mumunohanata@gmail.com>
2020-08-10 16:46:21 +0900

    TopMainView AutoLayout作成中

--- CountApp/CountApp/View/TopMainView.swift
+++ CountApp/CountApp/View/TopMainView.swift
@@ -13,28 +13,35 @@ protocol TopMainViewDelegate: NSObjectProtocol {
 }
 
 extension TopMainViewDelegate {
-    
-    
-    
 }
 
 // MARK: - Property
 class TopMainView: BaseView {
     weak var delegate: TopMainViewDelegate? = nil
     
-    @IBOutlet weak var countLavel: UILabel!
     
+    @IBOutlet weak var countLavel: UILabel!
+
+    
+    var count = 0
+
     
     @IBOutlet weak var pushButton: UIButton!
     
-
     
+    @IBAction func pushedButton(_ sender: UIButton) {
+        count = count + 1
+        countLavel.text = String(count)
+        
+    }
 }
 
 // MARK: - Life cycle
 extension TopMainView {
     override func awakeFromNib() {
         super.awakeFromNib()
+        countLavel.text = String(0)
+        countLavel.textColor = UIColor.white
     }
 }
 
